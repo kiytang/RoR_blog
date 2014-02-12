b33ed4a86da68a411c3109ca5866a9dc97a37940
Kiy Hong Tang <kiyhong@gmail.com>
2014-02-12 12:59:49 +0000

    added associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
+	has_many:comments
 #ensures that all posts have title that is at least 5 characters long	
 	validates :title, presence: true,
 						length: { minimum: 5}
+					[...]
 end
