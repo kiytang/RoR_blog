f74fb54a28549c8dab564488457819a279155051
Kiy Hong Tang <kiyhong@gmail.com>
2014-02-12 17:11:47 +0000

    added authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+	
+	http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = 
@@ -6,7 +9,9 @@ class CommentsController < ApplicationController
 		redirect_to post_path(@post)
 	end
 
-	def destroy
+# The destroy action will find the post we 
+# are looking at, locate the comment within the @post.comments collection, and then remove it from the database and send us back to the show action for the post.
+ 	def destroy
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.find(params[:id])
 		@comment.destroy
