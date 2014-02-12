882c7bb98b42e3d7c25a0f76f402a209c3e857b5
Kiy Hong Tang <kiyhong@gmail.com>
2014-02-12 17:16:14 +0000

    added authentication

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-	has_many :comments
+	has_many :comments, dependent: :destroy
 #ensures that all posts have title that is at least 5 characters long	
 	validates :title, presence: true,
 						length: { minimum: 5} 
